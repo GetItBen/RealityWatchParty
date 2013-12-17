54e59419f7fde1e30b397641b79581e0801742d8
Benjamin D. Walker <BennyBooBooBoo@gmail.com>
2013-12-16 19:59:21 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealityWatchParty::Application.config.secret_key_base = 'f819845c47ef45733ed651f95fee4b8614c3f5bd345295777de2dfc1b72d73498ed48d68bc0c8b0c40387de7a0dd2afc9823f140e6ce52a318875141f4931473'
